7aa2cf884c739affbf81e8d90226bbf9fef1ff49
MatroskeN <44238649+MatroskeN@users.noreply.github.com>
2021-04-05 18:36:43 +0400

    ап

--- app/js/main.js
+++ app/js/main.js
@@ -48,5 +48,22 @@ var $grid = $('.grid').isotope({
     });
   });
 
+  //counter
+  var time = 4;
+$('div').each(function(){
+  var i = 1,
+      num = $(this).data('num'),
+      step = 2000 * time / num,
+      that = $(this),
+      int = setInterval(function(){
+        if (i <= num) {
+          that.html(i);
+        }
+        else {
+          clearInterval(int);
+        }
+        i++;
+      },step);
+});
 })
 
